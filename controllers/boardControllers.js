d330af0216ac840c866522f6477cd7299817de3c
Leninbevan <charleslenin431@gmail.com>
2025-05-14 18:59:18 +0530

    added get api

--- controllers/boardControllers.js
+++ controllers/boardControllers.js
@@ -1,8 +1,17 @@
-import { generateBoard } from "../services/boardService.js";
+import { generateBoard, getBoard } from "../services/boardService.js";
 
 export const createBoard = async (req, res) => {
   try {
     const response = await generateBoard(req);
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+};
+
+export const getboard = async (req, res) => {
+  try {
+    const response = await getBoard(req);
     res.status(200).json(response);
   } catch (error) {
     res.status(400).json(error);
--- routes/boardRoutes.js
+++ routes/boardRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { createBoard } from "../controllers/boardControllers.js";
+import { createBoard, getboard } from "../controllers/boardControllers.js";
 
 const router = express.Router();
 
 // router.get("/getBoard",getboard);
 router.post("/createBoard", createBoard);
+router.get('/getBoardItems', getboard);
 
 export default router;
