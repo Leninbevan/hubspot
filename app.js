d85a66aba7ae44740a27117005f9b6066f18f03e
Praveen Saravanan <praveen.s@mitrahsoft.com>
2025-05-15 13:15:37 +0530

    Added the rate limitation in api

--- app.js
+++ app.js
@@ -3,11 +3,23 @@ import boardRouter from "./routes/boardRoutes.js";
 import swaggerJSDoc from 'swagger-jsdoc';
 import swaggerUI from 'swagger-ui-express';
 import dotenv from 'dotenv';
+import rateLimit from 'express-rate-limit'; 
+
 dotenv.config();
 
 const app = express();
-
 app.use(express.json());
+
+const limiter = rateLimit({
+  windowMs: 60 * 1000, 
+  max: 5,
+  message: {
+    code: 429,
+    message: "You have exceeded your five requests limit per minute !!!!!!!!!"
+  }
+});
+
+app.use(limiter);
 
 const options = {
   definition: {
@@ -44,6 +56,8 @@ app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 
 app.use("/api/v1/board", boardRouter);
 
+
 app.listen(8000, () => {
     console.log("Server is running on port 8000");
 });
+
